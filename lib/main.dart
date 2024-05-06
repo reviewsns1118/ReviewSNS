3da74daa300c87af942908bc93fdc59b9cd09ba5
reviewsns1118 <reviewsns1118@gmail.com>
2024-05-06 20:30:45 +0900

    returnで画面遷移してるせいでバグってた

--- lib/main.dart
+++ lib/main.dart
@@ -3,21 +3,30 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-import 'router.dart';
-import 'posts.dart';
-import 'writepost.dart';
-import 'add_work.dart';
-import 'UI.dart';
-import 'login.dart';
-import 'firebase_options.dart';
+import 'package:wotastagram/UI.dart';
+import 'package:wotastagram/add_work.dart';
+import 'package:wotastagram/firebase_options.dart';
+import 'package:wotastagram/infoupdate.dart';
+import 'package:wotastagram/login.dart';
+import 'package:wotastagram/othersAccount.dart';
+import 'package:wotastagram/posts.dart';
+import 'package:wotastagram/writepost.dart';
 
 final RouteObserver<ModalRoute> routeObserver = RouteObserver<ModalRoute>();
+bool login=false;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  FirebaseAuth.instance.authStateChanges().listen((User? user) {
+    if (user == null) {
+      login=false;
+    } else {
+      login=true;
+    }
+  });
 
   runApp(
     ProviderScope(
@@ -28,10 +37,9 @@ Future<void> main() async {
 
 
 
+
 class MyApp extends StatelessWidget {
   MyApp({super.key});
-
-  
 
   @override
   Widget build(BuildContext context) => MaterialApp.router(
@@ -46,28 +54,106 @@ class MyApp extends StatelessWidget {
       );
 }
 
-class InitialPage extends StatelessWidget{
-  @override
-  Widget build(BuildContext context){
-    return StreamBuilder<User?>(
-          stream: FirebaseAuth.instance.authStateChanges(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              // スプラッシュ画面などに書き換えても良い
-              return const SizedBox();
-            }
-            if (snapshot.hasData) {
-              // User が null でなない、つまりサインイン済みのホーム画面へ
-              context.goNamed(
-                'home',
-                extra: 0,
-              );
-              return UI(0);
-            }
-            // User が null である、つまり未サインインのサインイン画面へ
-            context.goNamed('start');
-            return StartPage();
+
+final goRouter = GoRouter(
+  debugLogDiagnostics: true,
+  initialLocation: login?"/home":"/start",
+  routes: [
+    GoRoute(
+      name: 'start',
+      path: '/start',
+      routes: [
+        GoRoute(
+          name: 'register',
+          path: 'register',
+          pageBuilder: (context, state) => MaterialPage(
+            key: state.pageKey,
+            child: RegisterPage(),
+          ),
+        ),
+        GoRoute(
+          name: 'login',
+          path: 'login',
+          pageBuilder: (context, state) => MaterialPage(
+            key: state.pageKey,
+            child: LoginPage(),
+          ),
+        ),
+      ],
+      pageBuilder: (context, state) => MaterialPage(
+        key: state.pageKey,
+        child: StartPage(),
+      ),
+    ),
+    GoRoute(
+      name: 'home',
+      path: '/home',
+      routes: [
+        GoRoute(
+          name: 'posts',
+          path: 'posts/:postid',
+          builder: (context, state) {
+            return Posts(state.pathParameters["postid"]);
           },
-        );
-}
-}
+        ),
+        GoRoute(
+          name: 'infoupdate',
+          path: 'infoupdate',
+          builder: (context, state) {
+            return Infoupdate();
+          },
+        ),
+        GoRoute(
+          name: 'addwork',
+          path: 'addwork',
+          routes: [
+            GoRoute(
+              name: 'writepostfromaddwork',
+              path: 'writepost',
+              builder: (context, state) {
+                return WritePost(state.extra as Map<String, dynamic>?);
+              },
+            ),
+          ],
+          builder: (context, state) {
+            return Add();
+          },
+        ),
+        GoRoute(
+          name: 'writepostfrompost',
+          path: 'writepost',
+          builder: (context, state) {
+            return WritePost(state.extra as Map<String, dynamic>?);
+          },
+        ),
+        GoRoute(
+          name: 'writepostfrompostpage',
+          path: 'writepost',
+          builder: (context, state) {
+            return WritePost(state.extra as Map<String, dynamic>?);
+          },
+        ),
+        GoRoute(
+          name: 'accounts',
+          path: 'accounts/:uid',
+          builder: (context, state) {
+            return OtherAccountPage(state.pathParameters["uid"]!);
+          },
+        ),
+      ],
+      builder: (context, state) {
+        int index = state.extra.hashCode;
+        return UI(index);
+      },
+    ),
+  ],
+  //遷移ページがないなどのエラーが発生した時に、このページに行く
+  errorPageBuilder: (context, state) => MaterialPage(
+    key: state.pageKey,
+    child: Scaffold(
+      body: Center(
+        child: Text(state.error.toString()),
+      ),
+    ),
+  ),
+);
